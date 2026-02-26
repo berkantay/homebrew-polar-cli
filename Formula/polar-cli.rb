c2bec45de35cd0a037a274efb37511f23ab279b7
Berkant Ay <berkantay.5@gmail.com>
2026-02-26 10:32:13 +0100

    Add polar-cli formula v1.0.0

---
+++ Formula/polar-cli.rb
@@ -0,0 +1,19 @@
+class PolarCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "CLI for Polar — manage products, customers, and webhooks from the terminal"
+  homepage "https://github.com/berkantay/polar-cli"
+  url "https://files.pythonhosted.org/packages/79/b1/a305206f0d73e08c6a3d891535f142371767c6d02ef28e84ab341c3cc626/polar_cli-1.0.0.tar.gz"
+  sha256 "214d5beb7a285db33a451ad207664e116a8c379aee16e5608f7ede743c3f7620"
+  license "Apache-2.0"
+
+  depends_on "python@3.12"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "polar", shell_output("#{bin}/polar --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# Homebrew Tap for Polar CLI
+
+## Installation
+
+```bash
+brew tap berkantay/polar-cli
+brew install polar-cli
+```
+
+## Usage
+
+```bash
+polar --help
+polar auth login
+polar products list
+```
+
+## Links
+
+- [polar-cli on PyPI](https://pypi.org/project/polar-cli/)
+- [polar-cli on GitHub](https://github.com/berkantay/polar-cli)
